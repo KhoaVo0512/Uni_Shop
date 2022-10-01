aea55fb0a028e2470a35712783a87b3fc9b96ba9
mylin <mylin@DESKTOP-VFOQC2P>
2022-10-01 21:14:07 +0700

    Add project files.

---
+++ Models/CheckOut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Uni_Shop.ModelDBs;
+
+namespace Uni_Shop.Models
+{
+    public class CheckOut
+    {
+        public IEnumerable<CartContent> cartcontentdetail { get; set; }
+        public NguoiDung nguoidungdetail { get; set; }
+        public double ThanhTien { get; set; }
+    }
+}
